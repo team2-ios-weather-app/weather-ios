f5d7079fc43a463e9aca199ac1260da59818505b
Jooyeong(Yugen) <supershicker@gmail.com>
2024-02-07 15:42:00 +0900

    [feature] UI 테스트 위치날씨 구현

--- weather-ios/weather-ios/MainViewController.swift
+++ weather-ios/weather-ios/MainViewController.swift
@@ -6,41 +6,127 @@
 //
 
 import UIKit
+import CoreLocation
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var locationManager: CLLocationManager!
+    var weatherService = WeatherService()
+    
+    private let temperatureLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 32, weight: .medium)
+        label.textAlignment = .center
+        label.textColor = .black
+        label.numberOfLines = 0
+        return label
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupLocationManager()
+        setupUI()
+    }
+    
+    private func setupLocationManager() {
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+    }
+    
+    private func setupUI() {
+        view.backgroundColor = .white
+        view.addSubview(temperatureLabel)
         
-        let requestTestButton: UIButton = {
-            let button = UIButton(type: .system, primaryAction: .init(handler: { _ in
-                Task {
-                    let crntWeather = await WeatherService().getCrntWeatherData("수영구")
-                    debugPrint(crntWeather ?? "")
-                    DispatchQueue.main.async {
-                        self.view.backgroundColor = .black
-                    }
-                }
-            }))
-            
-            button.setTitle("requestTestButton", for: .normal)
-            button.translatesAutoresizingMaskIntoConstraints = false
-            return button
-        }()
+        temperatureLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            temperatureLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            temperatureLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
         
-        view.addSubview(requestTestButton)
+        setupButtons()
+    }
+    
+    private func setupButtons() {
+        let currentLocationWeatherButton = UIButton(type: .system)
+        currentLocationWeatherButton.setTitle("현재 위치 날씨", for: .normal)
+        currentLocationWeatherButton.addTarget(self, action: #selector(updateCurrentWeather), for: .touchUpInside)
+        
+        let testLocationWeatherButton = UIButton(type: .system)
+        testLocationWeatherButton.setTitle("테스트 위치 날씨", for: .normal)
+        testLocationWeatherButton.addTarget(self, action: #selector(requestWeatherUpdate), for: .touchUpInside)
+        
+        view.addSubview(currentLocationWeatherButton)
+        view.addSubview(testLocationWeatherButton)
+        
+        currentLocationWeatherButton.translatesAutoresizingMaskIntoConstraints = false
+        testLocationWeatherButton.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            requestTestButton.topAnchor.constraint(equalTo: view.topAnchor),
-            requestTestButton.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            requestTestButton.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            requestTestButton.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            currentLocationWeatherButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            currentLocationWeatherButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+            
+            testLocationWeatherButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            testLocationWeatherButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50)
         ])
+    }
+    
+    @objc private func updateCurrentWeather() {
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    @objc private func requestWeatherUpdate() {
+        Task {
+            let crntWeather = await weatherService.getCrntWeatherDataForCity("사하구")
+            DispatchQueue.main.async {
+                self.updateWeatherInfo(crntWeather)
+            }
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            print("현재 위치: 위도 \(location.coordinate.latitude), 경도 \(location.coordinate.longitude)")
+            locationManager.stopUpdatingLocation()
+            
+            weatherService.getCrntWeatherDataForCoordinates(lat: location.coordinate.latitude, lon: location.coordinate.longitude) { [weak self] weatherData in
+                DispatchQueue.main.async {
+                    self?.updateWeatherInfo(weatherData)
+                }
+            }
+        }
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch manager.authorizationStatus {
+        case .authorizedWhenInUse, .authorizedAlways:
+            print("위치 서비스 권한이 허용됨")
+            locationManager.startUpdatingLocation()
+        case .notDetermined, .restricted, .denied:
+            print("위치 서비스 권한이 없거나 제한됨")
+        @unknown default:
+            print("알 수 없는 새로운 권한 상태")
+        }
+    }
+    
+    func updateWeatherInfo(_ weatherData: CrntWeatherData?) {
+        guard let weatherData = weatherData else {
+            temperatureLabel.text = "날씨 정보를 가져올 수 없습니다."
+            return
+        }
+        
+        let temperature = weatherData.main?.temp ?? 0
+        let weatherStatus = weatherData.weather?.first?.description ?? "정보 없음"
+        let cityName = weatherData.name ?? "알 수 없는 도시"
+        
+        temperatureLabel.text = "도시: \(cityName)\n온도: \(temperature)°C\n상태: \(weatherStatus)"
     }
 }
 
 @available(iOS 17, *)
 #Preview("", traits: .defaultLayout) {
-  return MainViewController()
+    return MainViewController()
 }
--- weather-ios/weather-ios/Models/WeatherService.swift
+++ weather-ios/weather-ios/Models/WeatherService.swift
@@ -14,9 +14,10 @@ class WeatherService {
 
 //MARK: - Requests
 extension WeatherService {
+    
     /// 도시 이름 넣으면 현재 날씨 데이터 반환
-    func getCrntWeatherData(_ cityName: String) async -> CrntWeatherData? {
-        guard let coordi = await WeatherService().getCoordinate(cityName) else { return nil }
+    func getCrntWeatherDataForCity(_ cityName: String) async -> CrntWeatherData? {
+        guard let coordi = await WeatherService().getCoordinateForCity(cityName) else { return nil }
         let urlString = "https://api.openweathermap.org/data/2.5/weather?lat=\(coordi.lat)&lon=\(coordi.lon)&appid=\(apiKey)&lang=kr&units=metric"
         guard let url = URL(string: urlString) else { return nil }
         do {
@@ -30,7 +31,7 @@ extension WeatherService {
     }
     
     /// 도시 이름 넣으면 좌표 데이터 반환
-    func getCoordinate(_ cityName: String) async -> Coordinate? {
+    func getCoordinateForCity(_ cityName: String) async -> Coordinate? {
         let urlString = "http://api.openweathermap.org/geo/1.0/direct?q=\(cityName)&limit=1&appid=\(apiKey)"
         guard let url = URL(string: urlString) else { return nil }
 
@@ -43,40 +44,26 @@ extension WeatherService {
             return nil
         }
     }
-}
-
-//MARK: - Helpers
-extension WeatherService {
-    static func testGetCrntWeatherData() -> CrntWeatherData { // 사용 예시: WeatherService.testGetCrntWeatherData()
-        return CrntWeatherData(coord: .init(lat: 129.0689, lon: 35.2100),
-                           weather: [.init(id: 804,
-                                           main: "Clouds",
-                                           description: "온흐림",
-                                           icon: "04n")],
-                           main: .init(temp: 276.57,
-                                       feelsLike: 273.43,
-                                       tempMin: 276.57,
-                                       tempMax: 276.57,
-                                       pressure: 1025,
-                                       humidity: 85,
-                                       seaLevel: 1025,
-                                       grndLevel: 1022),
-                           visibility: 10000,
-                           wind: .init(speed: 3.45,
-                                       deg: 19,
-                                       gust: 5.19),
-                           clouds: .init(all: 100),
-                           rain: nil,
-                           snow: nil,
-                           dt: 1707167722,
-                           sys: .init(type: nil,
-                                      id: nil,
-                                      country: "KR",
-                                      sunrise: 1707171588,
-                                      sunset: 1707209740),
-                           timezone: 32400,
-                           id: 1838519,
-                           name: "Busan",
-                           cod: 200)
+    
+    /// 위경도로 날씨 데이터 반환
+    func getCrntWeatherDataForCoordinates(lat: Double, lon: Double, completion: @escaping (CrntWeatherData?) -> Void) {
+        let urlString = "https://api.openweathermap.org/data/2.5/weather?lat=\(lat)&lon=\(lon)&appid=\(apiKey)&lang=\("kr")"
+        guard let url = URL(string: urlString) else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print(error)
+                return
+            }
+            
+            guard let data = data else { return }
+            
+            do {
+                let weatherResponse = try JSONDecoder().decode(CrntWeatherData.self, from: data)
+                completion(weatherResponse)
+            } catch {
+                print(error)
+            }
+        }.resume()
     }
 }
