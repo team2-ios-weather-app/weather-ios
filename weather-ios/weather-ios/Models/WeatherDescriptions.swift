92048634089c34623b14cefa479bb34ef85026c2
JiHoon Kang <deferare@icloud.com>
2024-02-15 12:03:51 +0900

    API 데이터 전부 영어로 받고 코드 내부적으로 한글로 처리하도록 함. WeatherService에 Description 영->한 변환 함수 만듬

---
+++ weather-ios/weather-ios/Models/WeatherDescriptions.swift
@@ -0,0 +1,12 @@
+//
+//  WeatherDescriptions.swift
+//  weather-ios
+//
+//  Created by JiHoon K on 2/15/24.
+//
+
+import Foundation
+
+struct WeatherDescriptions {
+    
+}
--- weather-ios/weather-ios/Models/WeatherService.swift
+++ weather-ios/weather-ios/Models/WeatherService.swift
@@ -19,7 +19,7 @@ extension WeatherService {
         if let regionName = regionName { coordinate = await getCoordinateFor(regionName) }
             
         if let coordinate = coordinate {
-            let urlString = "https://api.openweathermap.org/data/2.5/weather?lat=\(coordinate.lat)&lon=\(coordinate.lon)&appid=\(Secrets.openWeatherMapAPIKey)&lang=kr&units=\(unit.rawValue)"
+            let urlString = "https://api.openweathermap.org/data/2.5/weather?lat=\(coordinate.lat)&lon=\(coordinate.lon)&appid=\(Secrets.openWeatherMapAPIKey)&units=\(unit.rawValue)"
             guard let url = URL(string: urlString) else { return nil }
             do {
                 let (data, _) = try await URLSession.shared.data(from: url)
@@ -50,6 +50,68 @@ extension WeatherService {
             return nil
         }
     }
+    
+    func getTheDescriptionKorVer(_ engVer: String) -> String {
+        switch engVer {
+        case "clear sky": "맑은 하늘"
+        case "few clouds": "구름 조금"
+        case "scattered clouds": "조각 구름"
+        case "broken clouds": "부서진 구름"
+        case "shower rain": "소나기"
+        case "rain": "비"
+        case "thunderstorm": "천둥번개"
+        case "snow": "눈"
+        case "mist": "안개"
+        case "thunderstorm with light rain": "약한 비를 동반한 천둥번개"
+        case "thunderstorm with rain": "비를 동반한 천둥번개"
+        case "thunderstorm with heavy rain": "강한 비를 동반한 천둥번개"
+        case "light thunderstorm": "약한 천둥번개"
+        case "heavy thunderstorm": "강한 천둥번개"
+        case "ragged thunderstorm": "불규칙적인 천둥번개"
+        case "thunderstorm with light drizzle": "약한 이슬비를 동반한 천둥번개"
+        case "thunderstorm with drizzle": "이슬비를 동반한 천둥번개"
+        case "thunderstorm with heavy drizzle": "강한 이슬비를 동반한 천둥번개"
+        case "light intensity drizzle": "약한 이슬비"
+        case "drizzle": "이슬비"
+        case "heavy intensity drizzle": "강한 이슬비"
+        case "light intensity drizzle rain": "약한 이슬비 비"
+        case "drizzle rain": "이슬비 비"
+        case "heavy intensity drizzle rain": "강한 이슬비 비"
+        case "shower rain and drizzle": "소나기와 이슬비"
+        case "heavy shower rain and drizzle": "강한 소나기와 이슬비"
+        case "shower drizzle": "소나기 이슬비"
+        case "light rain": "약한 비"
+        case "moderate rain": "중간 비"
+        case "heavy intensity rain": "강한 비"
+        case "very heavy rain": "매우 강한 비"
+        case "extreme rain": "극심한 비"
+        case "freezing rain": "얼어붙는 비"
+        case "light intensity shower rain": "약한 소나기 비"
+        case "heavy intensity shower rain": "강한 소나기 비"
+        case "ragged shower rain": "불규칙적인 소나기 비"
+        case "light snow": "약한 눈"
+        case "heavy snow": "강한 눈"
+        case "sleet": "진눈깨비"
+        case "light shower sleet": "약한 진눈깨비 소나기"
+        case "shower sleet": "진눈깨비 소나기"
+        case "light rain and snow": "약한 비와 눈"
+        case "rain and snow": "비와 눈"
+        case "light shower snow": "약한 눈 소나기"
+        case "shower snow": "눈 소나기"
+        case "heavy shower snow": "강한 눈 소나기"
+        case "smoke": "연기"
+        case "haze": "안개"
+        case "sand/dust whirls": "모래/먼지 회오리"
+        case "fog": "안개"
+        case "sand": "모래"
+        case "dust": "먼지"
+        case "volcanic ash": "화산재"
+        case "squalls": "돌풍"
+        case "tornado": "토네이도"
+        default: ""
+        }
+    }
+
 }
 
 //MARK: - Helpers
@@ -64,7 +126,7 @@ extension WeatherService {
         return CrntWeatherData(coord: .init(lat: 35.1811, lon: 129.0928, localNames: .init(en: nil, ko: "연산동")),
                                weather: [.init(id: 803,
                                                main: "Clouds",
-                                               description: "튼구름",
+                                               description: "few clouds",
                                                icon: "04n")],
                                main: .init(temp: 5.4,
                                            feelsLike: 4.35,
